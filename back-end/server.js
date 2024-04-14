c5e4fb11e7a150b9d17b6761ca5e454df708b108
Ansh Jasrotia <anshvardhanavj@gmail.com>
2024-04-15 01:52:03 +0530

    FINAL COMMITS

--- back-end/server.js
+++ back-end/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-
+import path from "path";
 import authRoutes from "./routes/auth.routes.js"
 import messageRoutes from "./routes/message.routes.js"
 import userRoutes from "./routes/user.routes.js"
@@ -15,12 +15,20 @@ dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/front-end/dist")))
+
+app.get("*", (req, res) =>{
+    res.sendFile(path.join(__dirname, "front-end", "dist", "index.html"));
+})
 
 
 // app.get("/", (req, res) => {
