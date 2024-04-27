e093c0815d934e0c55d3a414f0e3360dfc72fde5
Ansh Jasrotia <anshvardhanavj@gmail.com>
2024-04-27 13:49:57 +0530

    resposniveness for below medium width devices

--- front-end/src/components/siderbar/Sidebar.jsx
+++ front-end/src/components/siderbar/Sidebar.jsx
@@ -4,11 +4,12 @@ import SearchInput from "./SearchInput"
 
 const Sidebar = () => {
   return (
-    <div className="border-r border-slate-500 p-4 flex flex-col">
+    <div className="border-r border-slate-500 p-4 flex flex-col max-md:h-[520px]">
         <SearchInput/>
         <div className="divider px-3"></div>
         <Conversations/>
         <LogoutButton/>
+        <div className="divider px-3 md:hidden"></div>
     </div>
   )
 }
--- front-end/src/pages/home/Home.jsx
+++ front-end/src/pages/home/Home.jsx
@@ -1,13 +1,13 @@
-import MessageContainer from "../../components/messages/MessageContainer"
-import Sidebar from "../../components/siderbar/Sidebar"
+import MessageContainer from "../../components/messages/MessageContainer";
+import Sidebar from "../../components/siderbar/Sidebar";
 
 const Home = () => {
   return (
-    <div className="flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-slate-900">
-      <Sidebar/>
-      <MessageContainer/>
+    <div className="flex max-md:flex-col md:h-[550px] max-md:h-[calc(100vh-20px)] max-md:w-[70vh] max-md:gap-4  overflow-x-hidden rounded-lg overflow-y-auto bg-slate-900 scrollbar-hide">
+      <Sidebar />
+      <MessageContainer />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
